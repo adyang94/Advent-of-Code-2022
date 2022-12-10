dd493dbfb36c9907cb3838f99f17c04ee43b1676
adyang94 <adyang94@yahoo.com>
2022-12-10 17:52:11 -0500

    starting day 9

--- Day9.js
+++ Day9.js
@@ -6,16 +6,23 @@ import { ReadTxtToArray, arraysEqual, create2dArray } from './helper.js';
 let input = ReadTxtToArray('sample.txt');
 let grid = createGrid(input);
 let start = [grid.length / 2, grid.length / 2];
+let headLocation = start;
+let tailLocation = start;
 
 console.table(grid);
 
 for (let i = 0; i < input.length; i++){
+    let [dir, steps] = input[i].split(' ');
+
     
 }
 
 
 
 
+function checkTailNexToHead(a, b, x, y) {
+
+}
 
 
 function createGrid(input) {

b6cdd481625d0644e569ebbb6946e877d41b2028
adyang94 <adyang94@yahoo.com>
2022-12-10 17:17:17 -0500

    starting day 9

---
+++ Day9.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import { ReadTxtToArray, arraysEqual, create2dArray } from './helper.js';
+
+// let input = ReadTxtToArray('Day9Input.txt');
+let input = ReadTxtToArray('sample.txt');
+let grid = createGrid(input);
+let start = [grid.length / 2, grid.length / 2];
+
+console.table(grid);
+
+for (let i = 0; i < input.length; i++){
+    
+}
+
+
+
+
+
+
+function createGrid(input) {
+    let longest = 0;
+    let arr;
+
+    for (let i = 0; i < input.length; i++){
+        let number = input[i].split(' ').pop();
+        longest += Number(number);
+    }
+
+    console.log('1--', longest);
+
+    arr = Array(2 * longest).fill(null).map(() => Array(2 * longest).fill(undefined));
+
+    return arr;
+}
